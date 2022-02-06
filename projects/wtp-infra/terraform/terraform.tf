90499fbe818aea75eed9f8ba71a9c9ffb6a29eed
Kevin McCartney <hello@kevinmccartney.is>
2022-02-06 16:26:05 -0500

    Adding backend info

--- projects/wtp-infra/terraform/terraform.tf
+++ projects/wtp-infra/terraform/terraform.tf
@@ -1,5 +1,9 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket = "wtp-state"
+    key    = "infra-terraform.tfstate"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
