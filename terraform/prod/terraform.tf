3729d694ffa2dc9c52c5a6c8745459f65c0da87b
Kevin McCartney <hello@kevinmccartney.is>
2022-02-06 12:30:41 -0500

    Adding pwd

--- terraform/prod/terraform.tf
+++ terraform/prod/terraform.tf
@@ -7,23 +7,8 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = var.wtp_aws_region
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "wtp-web-state"
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
