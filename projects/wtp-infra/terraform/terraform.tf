49ef115b5ca278e7ebc35b5e2571d2f47f80da39
Kevin McCartney <hello@kevinmccartney.is>
2022-02-06 15:58:40 -0500

    Adding state lock table and moving tf files in infra

--- projects/wtp-infra/terraform/terraform.tf
+++ projects/wtp-infra/terraform/terraform.tf
@@ -24,3 +24,13 @@ resource "aws_s3_bucket" "terraform_state" {
     }
   }
 }
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "wtp-web-infra-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
