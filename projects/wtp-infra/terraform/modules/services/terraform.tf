922bb89b598abbd51aa373403efe3f5ce527ef2e
Kevin McCartney <hello@kevinmccartney.is>
2022-02-07 01:07:46 -0500

    Setting gateway account

--- projects/wtp-infra/terraform/modules/services/terraform.tf
+++ projects/wtp-infra/terraform/modules/services/terraform.tf
@@ -36,6 +36,57 @@ resource "aws_apigatewayv2_stage" "default" {
   }
 }
 
+resource "aws_api_gateway_account" "cloudwatch" {
+  cloudwatch_role_arn = aws_iam_role.cloudwatch.arn
+}
+
+resource "aws_iam_role" "api_gateway_cloudwatch" {
+  name = "api_gateway_cloudwatch"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "cloudwatch" {
+  name = "default"
+  role = aws_iam_role.api_gateway_cloudwatch.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:PutLogEvents",
+                "logs:GetLogEvents",
+                "logs:FilterLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+
 resource "aws_cloudwatch_log_group" "default" {
   name = "wtp-services-gateway-access-log"
 }
