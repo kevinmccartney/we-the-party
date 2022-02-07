ddcd1652f71288ebd40a6b3b55c05c5d65893533
Kevin McCartney <hello@kevinmccartney.is>
2022-02-07 18:36:21 -0500

    Adding method reponse and integration response

--- projects/wtp-infra/terraform/modules/services/terraform.tf
+++ projects/wtp-infra/terraform/modules/services/terraform.tf
@@ -52,6 +52,24 @@ resource "aws_api_gateway_method" "ping_get" {
   authorization = "NONE"
 }
 
+resource "aws_api_gateway_method_response" "ping_200" {
+  # depends_on  = ["aws_api_gateway_method.api-method", "aws_api_gateway_integration.api-method-integration"]
+  rest_api_id   = aws_api_gateway_rest_api.services.id
+  resource_id   = aws_api_gateway_resource.ping.id
+  http_method   = "GET"
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
 resource "aws_api_gateway_integration" "ping_get" {
   rest_api_id = aws_api_gateway_rest_api.services.id
   resource_id = aws_api_gateway_resource.ping.id
@@ -73,11 +91,21 @@ TEMPLATE
   }
 }
 
-resource "aws_api_gateway_method_response" "response_200" {
-  rest_api_id = aws_api_gateway_rest_api.services.id
-  resource_id = aws_api_gateway_resource.ping.id
-  http_method = aws_api_gateway_method.ping_get.http_method
+resource "aws_api_gateway_integration_response" "ping_200" {
+  # depends_on  = ["aws_api_gateway_method_response.ok", "aws_api_gateway_method.api-method", "aws_api_gateway_integration.api-method-integration"]
+  resource_id   = aws_api_gateway_resource.ping.id
+  http_method   = "GET"
   status_code = "200"
+
+  response_templates = {
+    "application/json" = ""
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET'"
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
 }
 
 resource "aws_api_gateway_stage" "v1" {
