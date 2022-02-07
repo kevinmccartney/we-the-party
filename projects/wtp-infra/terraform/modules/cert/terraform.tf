2e8e2ea768918572d93c7d779ab8e720833a8827
Kevin McCartney <hello@kevinmccartney.is>
2022-02-06 20:09:41 -0500

    Fixing cert reference

--- projects/wtp-infra/terraform/modules/cert/terraform.tf
+++ projects/wtp-infra/terraform/modules/cert/terraform.tf
@@ -28,7 +28,7 @@ resource "aws_route53_record" "validation" {
   depends_on = [aws_acm_certificate.default]
 
   for_each = {
-    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.default.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
