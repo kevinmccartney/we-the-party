5eeaac47094021c03c3ff6ab7be3825937f8b09f
Kevin McCartney <hello@kevinmccartney.is>
2022-02-07 02:03:29 -0500

    Adding deployment

--- projects/wtp-infra/terraform/modules/services/terraform.tf
+++ projects/wtp-infra/terraform/modules/services/terraform.tf
@@ -19,3 +19,15 @@ resource "aws_route53_record" "services" {
     zone_id                = aws_api_gateway_domain_name.services.cloudfront_zone_id
   }
 }
+
+resource "aws_api_gateway_deployment" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.services))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
