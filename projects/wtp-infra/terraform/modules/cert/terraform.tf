32c87d804edb7bf922346f3644918f3c3d7fca8c
Kevin McCartney <hello@kevinmccartney.is>
2022-02-06 20:23:26 -0500

    Removing hello world, echoing sans

--- projects/wtp-infra/terraform/modules/cert/terraform.tf
+++ projects/wtp-infra/terraform/modules/cert/terraform.tf
@@ -1,52 +1,60 @@
-resource "aws_route53_zone" "domain_routes" {
-  name = "wethe.party"
-  tags = {
-    "project" = "we-the-party",
-    "managed_by" = "terraform"
-  }
+locals {
+  wtp_sans = concat([for domain, key in var.domain_names : "*.${domain}" if key != "infra"], [for name in var.domain_names : domain if key != "infra"])
 }
 
-resource "aws_acm_certificate" "default" {
-  domain_name               = var.domain_names["apex"]
-  subject_alternative_names = [for name in var.domain_names : "*.${name}"]
-  validation_method         = "DNS"
-  options {
-    certificate_transparency_logging_preference = "ENABLED"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags                      = {
-    "project" = "we-the-party",
-    "managed_by" = "terraform"
-  }
+output "wtp_sans" {
+  value = local.wtp_sans
 }
 
-resource "aws_route53_record" "validation" {
-  depends_on = [aws_acm_certificate.default]
+# resource "aws_route53_zone" "domain_routes" {
+#   name = "wethe.party"
+#   tags = {
+#     "project" = "we-the-party",
+#     "managed_by" = "terraform"
+#   }
+# }
 
-  for_each = {
-    for dvo in aws_acm_certificate.default.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+# resource "aws_acm_certificate" "default" {
+#   domain_name               = var.domain_names["apex"]
+#   subject_alternative_names = local.wtp_sans
+#   validation_method         = "DNS"
+#   options {
+#     certificate_transparency_logging_preference = "ENABLED"
+#   }
 
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id = aws_route53_zone.domain_routes.zone_id
-  allow_overwrite = true
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
 
-resource "aws_acm_certificate_validation" "default" {
-  count = length(aws_route53_record.validation)
+#   tags                      = {
+#     "project" = "we-the-party",
+#     "managed_by" = "terraform"
+#   }
+# }
 
-  certificate_arn         = aws_acm_certificate.default.arn
+# resource "aws_route53_record" "validation" {
+#   depends_on = [aws_acm_certificate.default]
+
+#   for_each = {
+#     for dvo in aws_acm_certificate.default.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     }
+#   }
+
+#   name            = each.value.name
+#   records         = [each.value.record]
+#   ttl             = 60
+#   type            = each.value.type
+#   zone_id = aws_route53_zone.domain_routes.zone_id
+#   allow_overwrite = true
+# }
+
+# resource "aws_acm_certificate_validation" "default" {
+#   count = length(aws_route53_record.validation)
+
+#   certificate_arn         = aws_acm_certificate.default.arn
   
-  validation_record_fqdns = [for validation in aws_route53_record.validation: validation.fqdn]
-}
+#   validation_record_fqdns = [for validation in aws_route53_record.validation: validation.fqdn]
+# }
