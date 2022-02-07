94ca9503035c41a65833bad97eb74b65b60a6540
Kevin McCartney <hello@kevinmccartney.is>
2022-02-07 01:51:56 -0500

    Setting custom domain name

--- projects/wtp-infra/terraform/modules/services/terraform.tf
+++ projects/wtp-infra/terraform/modules/services/terraform.tf
@@ -1,25 +1,21 @@
-resource "aws_route53_record" "services_gateway" {
-  name    = aws_apigatewayv2_domain_name.services.domain_name
-  zone_id = var.wtp_route_53_zone_id
-  type    = "A"
-
-  alias {
-    name                   = aws_apigatewayv2_domain_name.services.domain_name_configuration[0].target_domain_name
-    zone_id                = aws_apigatewayv2_domain_name.services.domain_name_configuration[0].hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_apigatewayv2_domain_name" "services" {
-  domain_name = "infra.wethe.party"
-
-  domain_name_configuration {
-    certificate_arn = var.wtp_cert_arn
-    endpoint_type   = "REGIONAL"
-    security_policy = "TLS_1_2"
-  }
-}
-
 resource "aws_api_gateway_rest_api" "services" {
   name = "wtp-service"
+}
+
+resource "aws_api_gateway_domain_name" "services" {
+  certificate_arn = var.wtp_cert_arn
+  domain_name     = "infra.wethe.party"
+  security_policy = "TLS_1_2"
+}
+
+resource "aws_route53_record" "services" {
+  name    = aws_api_gateway_domain_name.services.domain_name
+  type    = "A"
+  zone_id = var.wtp_route_53_zone_id
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_api_gateway_domain_name.services.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.services.cloudfront_zone_id
+  }
 }
