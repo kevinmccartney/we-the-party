5171f773d75e5c28b9dfa0a6d8d3cab7fa175637
Kevin McCartney <hello@kevinmccartney.is>
2022-02-06 21:49:44 -0500

    Removing region var

--- projects/wtp_infra/terraform/modules/services/variables.tf
+++ projects/wtp_infra/terraform/modules/services/variables.tf
@@ -1,8 +1,3 @@
-variable "wtp_aws_region" {
-  type = string
-  description = "The AWS region in which our serverless resources reside"
-}
-
 variable "wtp_route_53_zone_id" {
   type = string
   description = "Id for the primary wtp Route 53 hosted zone"
