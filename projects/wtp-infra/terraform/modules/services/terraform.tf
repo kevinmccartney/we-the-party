f662024290c60cb765fb10b096ffc4dc6692a314
Kevin McCartney <hello@kevinmccartney.is>
2022-02-07 02:04:18 -0500

    fixing reference

--- projects/wtp-infra/terraform/modules/services/terraform.tf
+++ projects/wtp-infra/terraform/modules/services/terraform.tf
@@ -21,7 +21,7 @@ resource "aws_route53_record" "services" {
 }
 
 resource "aws_api_gateway_deployment" "example" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
+  rest_api_id = aws_api_gateway_rest_api.services.id
 
   triggers = {
     redeployment = sha1(jsonencode(aws_api_gateway_rest_api.services))
