3728b3b7df940cb8f618d3600bcd74552c6c138a
Kevin McCartney <hello@kevinmccartney.is>
2022-02-24 12:44:03 -0500

    Re-adding ping lambda

--- projects/wtp_infra/terraform/modules/services/terraform.tf
+++ projects/wtp_infra/terraform/modules/services/terraform.tf
@@ -131,17 +131,17 @@ resource "aws_iam_role_policy_attachment" "aws-lambda-basic-execution-role" {
   policy_arn = data.aws_iam_policy.AWSLambdaBasicExecutionRole.arn
 }
 
-# resource "aws_lambda_function" "ping" {
-#   s3_bucket     = "wtp-infra-lambdas"
-#   s3_key        = "ping.zip"
-#   function_name = "wtp_services_ping"
-#   role          = aws_iam_role.lambda_execution.arn
-#   handler       = "ping.lambda_handler"
+resource "aws_lambda_function" "ping" {
+  s3_bucket     = "wtp-infra-lambdas"
+  s3_key        = "ping.zip"
+  function_name = "wtp_services_ping"
+  role          = aws_iam_role.lambda_execution.arn
+  handler       = "ping.lambda_handler"
 
-#   source_code_hash = filebase64sha256("ping.zip")
+  source_code_hash = filebase64sha256("ping.zip")
 
-#   runtime = "python3.9"
-# }
+  runtime = "python3.9"
+}
 
 resource "aws_s3_bucket" "infra_lambdas" {
   bucket = "wtp-infra-lambdas"
