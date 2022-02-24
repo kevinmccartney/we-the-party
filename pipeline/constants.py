80c27edc8bbd0b78b383eb57d4e9821f94293670
Kevin McCartney <hello@kevinmccartney.is>
2022-02-24 00:43:17 -0500

    Starting to work on dynamic conf generaation

---
+++ pipeline/constants.py
@@ -0,0 +1,18 @@
+PROJECTS = {
+    "wtp_admin": {
+        "src": "projects/wtp_admin/src",
+        "terraform": "projects/wtp_admin/terraform",
+    },
+    "wtp_api": {
+        "src": "projects/wtp_api/src",
+        "terraform": "projects/wtp_api/terraform",
+    },
+    "wtp_client": {
+        "src": "projects/wtp_client/src",
+        "terraform": "projects/wtp_client/terraform",
+    },
+    "wtp_infra": {
+        "src": "projects/wtp_infra/src",
+        "terraform": "projects/wtp_infra/terraform",
+    },
+}
